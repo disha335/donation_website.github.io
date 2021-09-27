baaccc0baeb1eec593f2b34dffcb98f891cbeb3f
Disha Handa <53966546+disha335@users.noreply.github.com>
2021-09-27 14:36:27 +0530

    Donation portal
    Donation portal for education of girl-child

---
+++ instamojoproject/pay.php
@@ -0,0 +1,26 @@
+<?php
+require('./instamojo.php');
+const API_KEY ="test_03a0d79eb529cf03ecb0f7f4edd";
+const AUTH_TOKEN = "test_1d8b299643d6ea53cbac4bfff99";
+
+
+if(isset($_POST['purpose']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['amount']))
+{
+    $api = new Instamojo\Instamojo(API_KEY, AUTH_TOKEN,'https://test.instamojo.com/api/1.1/');
+    
+    try {
+        $response = $api->paymentRequestCreate(array(
+            "purpose" => $_POST['purpose'],
+            "buyer_name" => $_POST['name'],
+            "amount" => $_POST['amount'],
+            "send_email" => true,
+            "email" => $_POST['email'],
+            "redirect_url" => "http://localhost/instamojoproject/success.html"
+            ));
+        header('Location:'. $response['longurl']);
+    }
+    catch (Exception $e) {
+        print('Error: ' . $e->getMessage());
+    }
+}
+?>
